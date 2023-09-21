c260deb1a24e0dae221f4994f488dee7ab39e7b1
cola639 <337507950@qq.com>
2023-09-21 15:45:05 +0800

    perf

--- sshScript/warp_cloudfare/warp_open.sh
+++ sshScript/warp_cloudfare/warp_open.sh
@@ -9,6 +9,9 @@ echo $ip_addr
 # 添加排除路由
 warp-cli add-excluded-route $ip_addr
 
+# 添加所有路由连接IPV4
+warp-cli add-excluded-route 0.0.0.0/0
+
 # 查看当前排除路由列表
 # warp-cli excluded-routes
 warp-cli get-excluded-routes
