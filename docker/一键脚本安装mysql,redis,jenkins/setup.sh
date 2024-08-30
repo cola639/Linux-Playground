0c9b68ad6e16819cd7f27f9ca5ed3b7253a499d7
cola639 <337507950@qq.com>
2024-08-31 04:26:45 +0800

    优化Docker和DockerCompose的安装脚本

--- docker/一键脚本安装mysql,redis,jenkins/setup.sh
+++ docker/一键脚本安装mysql,redis,jenkins/setup.sh
@@ -1,16 +1,29 @@
-# 安装docker
-sudo apt-get update &&
-  sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common &&
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+#!/bin/bash
+
+# 检查 Docker 是否已安装
+if ! command -v docker &>/dev/null; then
+  echo "Docker 未安装，正在安装 Docker..."
   sudo apt-get update &&
-  sudo apt-get install -y docker-ce &&
-  docker --version &&
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common &&
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
+    sudo apt-get update &&
+    sudo apt-get install -y docker-ce &&
+    docker --version
+else
+  echo "Docker 已安装，跳过 Docker 安装步骤。"
+fi
 
-  # 安装docker-compose
+# 检查 Docker Compose 是否已安装
+if ! command -v docker-compose &>/dev/null; then
+  echo "Docker Compose 未安装，正在安装 Docker Compose..."
   sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
-  sudo chmod +x /usr/local/bin/docker-compose &&
-  docker-compose --version &&
+    sudo chmod +x /usr/local/bin/docker-compose &&
+    docker-compose --version
+else
+  echo "Docker Compose 已安装，跳过 Docker Compose 安装步骤。"
+fi
 
-  # 安装docker-compose.yml配置
-  docker-compose -f docker-compose.yml up -d
+# 运行 docker-compose.yml
+echo "安装 mysql redis jenkins 配置..."
+docker-compose -f docker-compose.yml up -d
